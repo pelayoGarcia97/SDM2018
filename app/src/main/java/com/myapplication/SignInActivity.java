d4345b5991a75b221840354b5e90706251271920
Óscar Marín Iglesias <UO251857@uniovi.es>
2018-10-20 21:07:22 +0200

    Añadidas Actividades

--- app/src/main/java/com/myapplication/SignInActivity.java
+++ app/src/main/java/com/myapplication/SignInActivity.java
@@ -7,12 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class SignIn extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        setTitle("Sign In");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
